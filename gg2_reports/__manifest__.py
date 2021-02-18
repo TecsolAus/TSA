20309c7dd7aebdde2604ad1fc57e777b9cd40b80
GraamGunn <graham@iq4.com.au>
2021-02-18 11:41:06 +1100

    Initial creation of gg2_reports

---
+++ gg2_reports/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "gg2_reports",
+
+    'summary': """
+        TSA Customisations - reports""",
+
+    'description': """
+        TSA Customisations - reports
+    """,
+
+    'author': "Graham Gunn,  IQ4",
+    'website': "http://www.iq4.com.au",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    # Developed under the following version of Odoo
+    'version': '14.0.1.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'account', 'product'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': [],
+    "images":['static/description/Banner.png'],
+}
