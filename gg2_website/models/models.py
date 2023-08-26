7e417e4f760d14d29955824920f8d6dd553770aa
TecsolAus <17488416+TecsolAus@users.noreply.github.com>
2023-08-26 10:21:02 +1000

    Delete gg2_website directory

--- gg2_website/models/models.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from typing import Any, Union
-
-from odoo import models, fields, api, tools, _
-from odoo.exceptions import UserError
-import pdb
-from mock.mock import self
